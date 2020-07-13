a40b0292a8b0a3303af8db21ed95314dc9dd039e
CodingSeb <sebgeiser@gmail.com>
2020-07-13 08:24:06 +0200

    Separate Operators and create basic Token

---
+++ CodingSeb.ExpressionEvaluator/CSEEToken.cs
@@ -0,0 +1,25 @@
+﻿/******************************************************************************************************
+    Project : ExpressionEvaluator (https://github.com/codingseb/ExpressionEvaluator)
+
+    Author : Coding Seb
+    Licence : MIT (https://github.com/codingseb/ExpressionEvaluator/blob/master/LICENSE.md)
+*******************************************************************************************************/
+
+namespace CodingSeb.ExpressionEvaluator
+{
+    /// <summary>
+    /// Represent a ExpressionEvaluator language token. The basic element for the syntaxic tree.
+    /// </summary>
+    public abstract partial class CSEEToken
+    {
+        /// <summary>
+        /// The sub text part of the expression that represent this token
+        /// </summary>
+        public string CapturedText { get; set; }
+
+        /// <summary>
+        /// The position of this token in it's parent expression
+        /// </summary>
+        public int Position { get; set; }
+    }
+}
